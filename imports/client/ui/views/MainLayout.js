9346dcb7ec0ef8ac18e9dbbb915d80641e0d0755
zhaoyao91 <zhaoyao91@163.com>
2017-06-17 02:53:44 +0800

    Fix navbar

--- imports/client/ui/views/MainLayout.js
+++ imports/client/ui/views/MainLayout.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { compose } from 'recompose'
 
 import MainNavbar from './MainNavbar'
+import withStyles from '../../hocs/with_styles'
 
-export default function MainLayout ({children}) {
-  return <div>
-    <div><MainNavbar/></div>
+export default compose(
+  withStyles('styles', {
+    layout: {
+      paddingTop: '54px'
+    },
+    navbarWrapper: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      zIndex: 1,
+    }
+  })
+)(function MainLayout ({styles, children}) {
+  return <div {...styles.layout}>
+    <div {...styles.navbarWrapper}>
+      <MainNavbar/>
+    </div>
     <div>{children}</div>
   </div>
-}
+})
